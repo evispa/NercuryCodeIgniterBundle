0630cfc742b58643d9cb112ac10c7d8c90c5ddea
Nerijus Arlauskas <nercury@gmail.com>
2012-11-16 14:43:52 +0200

    Fixed relative path function.

--- CiHelperService.php
+++ CiHelperService.php
@@ -92,29 +92,26 @@ class CiHelperService {
     }
 
     function getRelativePath($from, $to) {
-        $from = explode('/', $from);
-        $to = explode('/', $to);
-        $relPath = $to;
-
-        foreach ($from as $depth => $dir) {
-            // find first non-matching dir
-            if ($dir === $to[$depth]) {
-                // ignore this directory
-                array_shift($relPath);
-            } else {
-                // get number of remaining dirs to $from
-                $remaining = count($from) - $depth;
-                if ($remaining > 1) {
-                    // add traversals up to first matching dir
-                    $padLength = (count($relPath) + $remaining - 1) * -1;
-                    $relPath = array_pad($relPath, $padLength, '..');
-                    break;
-                } else {
-                    $relPath[0] = $relPath[0];
-                }
-            }
+        $patha = explode('/', $from);
+        $pathb = explode('/', $to);
+        
+        $start_point = count(array_intersect($patha,$pathb));
+        
+        while($start_point--) {
+            array_shift($patha);
+            array_shift($pathb);
         }
-        return implode('/', $relPath);
+        
+        $output = "";
+        if(($back_count = count($patha))) {
+            while($back_count--) {
+                $output .= "../";
+            }
+        } else {
+            $output .= './';
+        }
+        
+        return $output . implode('/', $pathb);
     }
 
     private $paths_initalized = false;
